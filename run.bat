cbda4ed820a4b6c1b5dd80e8ea690a167a27d8a2
Chisaki-Meteora <Chisaki-Meteora@github.com>
2021-03-06 15:03:26 +0800

    公告更新

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@ cd C:\Users\Administrator\Desktop\githubc\NEWS\download
 wget https://alice.colopl.jp/news/importants
 move importants C:\Users\Administrator\Desktop\githubc\NEWS
 call git add -A 
-call git commit -m "有更新"
+call git commit -m "公告更新"
 call git push -u origin main
 
 
