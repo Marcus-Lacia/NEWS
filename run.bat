dc677028aeb63fd6f6ecbd3e0d05d0be42ffdf28
Marcus-Lacia <marcuslacia@qq.com>
2021-03-06 16:04:04 +0800

    公告更新

--- run.bat
+++ run.bat
@@ -1,8 +1,8 @@
 cd C:\Users\Administrator\Desktop\githubc\NEWS\download
 python aga.py
 move agaim.txt C:\Users\Administrator\Desktop\githubc\NEWS
-call git add -A 
-call git commit -m "公告更新"
-call git push -u origin main
+git add -A 
+git commit -m "公告更新"
+git push -u origin main
 
 
