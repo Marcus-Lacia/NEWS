4feb903622aad927bb08c18604f0f7dcc5831903
Chisaki-Meteora <Chisaki-Meteora@github.com>
2021-03-06 13:45:28 +0800

    有更新

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
-cd download
-aria2c https://alice.colopl.jp/news/importants
+cd C:\Users\Administrator\Desktop\githubc\NEWS\download
+wget https://alice.colopl.jp/news/importants
 move importants C:\Users\Administrator\Desktop\githubc\NEWS
 call git add -A 
 call git commit -m "ÓÐ¸üÐÂ"
