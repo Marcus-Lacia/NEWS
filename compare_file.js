3f9501490d654f2bb7edd37989d1dfa604a5e813
Marcus-Lacia <marcuslacia@qq.com>
2021-03-09 17:25:09 +0800

    公告更新

--- compare_file.js
+++ compare_file.js
@@ -1,10 +1,11 @@
-﻿/**
+/**
  * 使用:
  * 直接 node compare_file.js
  */
 const fs = require('fs');
-const spawn = require("child_process").spawn;
+const { spawn } = require('child_process');
 
+// 檔案路徑
 const filePath1 = 'C:/Users/Administrator/Desktop/githubc/NEWS/beta/new.txt';
 const filePath2 = 'C:/Users/Administrator/Desktop/githubc/NEWS/new/new.txt';
 
@@ -13,5 +14,5 @@ const content2 = fs.readFileSync(filePath2, 'utf8');
 if (content1 == content2) {
     console.log('Both file are same');
 } else {
-    python 6.py;
+    const python = spawn('python', ["6.py"]);
 }
