04afe10ba109489be535f17e58423c91f32e4ecd
Marcus-Lacia <marcuslacia@qq.com>
2021-03-09 17:12:00 +0800

    公告更新

--- 3.sh
+++ 3.sh
@@ -2,7 +2,8 @@ file1=C:/Users/Administrator/Desktop/githubc/NEWS/beta/new.txt
 file2=C:/Users/Administrator/Desktop/githubc/NEWS/new/new.txt
  
 diff $file1 $file2 > /dev/null
-if [ $0 == 0 ]; then
+if [ $0 == 0 ]; 
+then
     echo "Both file are same"
 else   
 	echo "python 6.py"
--- r1un.bat
+++ r1un.bat
@@ -1,12 +1,12 @@
 cd ..
 cd C:\Users\Administrator\Desktop\githubc\NEWS\download
 python aga.py
-ping 127.0.0.1 
+
 
 move agaim.txt C:\Users\Administrator\Desktop\githubc\NEWS
 cd C:\Users\Administrator\Desktop\githubc\NEWS
 node chs.js
-ping 127.0.0.1 
+
 
 cd C:\Users\Administrator\Desktop\githubc\NEWS\beta
 rename *.txt new.txt
@@ -15,7 +15,7 @@ rename *.txt new.txt
 cd C:\Users\Administrator\Desktop\githubc\NEWS
 sh ./3.sh
 
-ping 127.0.0.1 
+
 git add -A 
 git commit -m "公告更新"
 git push -u origin main
