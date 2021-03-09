3fdc1f33a34245d8a1b8c7b5d47426f72e5760f3
Marcus-Lacia <marcuslacia@qq.com>
2021-03-09 14:21:24 +0800

    ¹«¸æ¸üÐÂ

--- 1run.sh
+++ 1run.sh
@@ -3,8 +3,8 @@ python aga.py
 move agaim.txt C:\Users\Administrator\Desktop\githubc\NEWS
 cd C:\Users\Administrator\Desktop\githubc\NEWS
 node chs.js
-call rename.bat
-git bash 3.sh
+bash rename.bat
+bash 3.sh
 git add -A 
 git commit -m "公告更新"
 git push -u origin main
