035d6a2287f7bf226e62ba74ae260c9cf576574a
Marcus-Lacia <marcuslacia@qq.com>
2021-03-09 14:17:56 +0800

    公告更新

--- 1run.bat
+++ 1run.bat
@@ -3,8 +3,8 @@ python aga.py
 move agaim.txt C:\Users\Administrator\Desktop\githubc\NEWS
 cd C:\Users\Administrator\Desktop\githubc\NEWS
 node chs.js
-bash rename.bat
-bash 3.sh
+call rename.bat
+call 3.sh
 git add -A 
 git commit -m "公告更新"
 git push -u origin main
