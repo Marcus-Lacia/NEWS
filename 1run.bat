9d4dc1bcf406fef96497bdf4de98dffe545abd62
Marcus-Lacia <marcuslacia@qq.com>
2021-03-09 14:19:32 +0800

    公告更新

--- 1run.bat
+++ 1run.bat
@@ -4,7 +4,7 @@ move agaim.txt C:\Users\Administrator\Desktop\githubc\NEWS
 cd C:\Users\Administrator\Desktop\githubc\NEWS
 node chs.js
 call rename.bat
-bash 3.sh
+git bash 3.sh
 git add -A 
 git commit -m "公告更新"
 git push -u origin main
