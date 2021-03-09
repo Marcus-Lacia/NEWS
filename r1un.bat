7b462936b183bf9b2da41e81d3f5d93d1c947220
Marcus-Lacia <marcuslacia@qq.com>
2021-03-09 16:59:47 +0800

    公告更新

--- r1un.bat
+++ r1un.bat
@@ -1,15 +1,21 @@
 cd ..
 cd C:\Users\Administrator\Desktop\githubc\NEWS\download
 python aga.py
+ping 127.0.0.1 
+
 move agaim.txt C:\Users\Administrator\Desktop\githubc\NEWS
 cd C:\Users\Administrator\Desktop\githubc\NEWS
 node chs.js
+ping 127.0.0.1 
+
 cd C:\Users\Administrator\Desktop\githubc\NEWS\beta
 rename *.txt new.txt
 cd C:\Users\Administrator\Desktop\githubc\NEWS\new
 rename *.txt new.txt
 cd C:\Users\Administrator\Desktop\githubc\NEWS
 sh ./3.sh
+
+ping 127.0.0.1 
 git add -A 
 git commit -m "公告更新"
 git push -u origin main
