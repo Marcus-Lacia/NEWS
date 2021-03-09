4139e4e42f725545cf232b413a3bddb59ca107c8
Marcus-Lacia <marcuslacia@qq.com>
2021-03-09 17:14:06 +0800

    公告更新

--- 3.sh
+++ 3.sh
@@ -1,5 +1,5 @@
 file1=C:\Users\Administrator\Desktop\githubc\NEWS\beta\new.txt
-file2=C:\sers\Administrator\Desktop\githubc\NEWS\new\new.txt
+file2=C:\Users\Administrator\Desktop\githubc\NEWS\new\new.txt
  
 diff $file1 $file2 > \dev\null
 if [ $0 == 0 ]; 
