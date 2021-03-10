2c45bee2197b724f2ea54460bd6a8d177bd726ce
Marcus-Lacia <marcuslacia@qq.com>
2021-03-10 12:07:42 +0800

    公告更新

--- 6.py
+++ 6.py
@@ -1,56 +1,38 @@
 import requests
 import shutil
 import os
+import time 
 G = ("C:/Users/Administrator/Desktop/githubc/NEWS/new/new.txt")  
 C = ("C:/Users/Administrator/Desktop/githubc/NEWS/beta/new.txt")
 D = ("C:/Users/Administrator/Desktop/githubc/NEWS/new/new.txt")
 B = ("C:/Users/Administrator/Desktop/githubc/NEWS/new/new.txt")
-Z = 0
+
 H =["970916689","894769419","602762228"]
 L =["DesignBy @Lacia  AssistBy @KINGMAX","自动机翻"]
 
-if Z<4 :
-    shutil.copyfile(C,D) 
+Z = 2
 
-    S = requests.Session()
-    URL = "http://127.0.0.1:5700/send_group_msg"
+shutil.copyfile(C,D) 
 
-    A = open(B,'rb').read()
+S = requests.Session()
+URL = "http://127.0.0.1:5700/send_group_msg"
 
-    PARAMS_1 = {
+A = open(B,'rb').read()
+
+PARAMS_1 = {
         "group_id": H[Z],
         "message": A,
-    }
-    R = S.post(url=URL, params=PARAMS_1)
-    DATA = R.json()
-    print(DATA)
-    PARAMS_tr2 = {
+}
+R = S.post(url=URL, params=PARAMS_1)
+DATA = R.json()
+print(DATA)
+PARAMS_2 = {
             "group_id": H[Z],
             "message": L[0],
 
-    }
-    R = S.post(url=URL, params=PARAMS_2)
-    DATA = R.json()
-    print(DATA)        
-    os.remove(C) 
-    os.system('python C:/Users/Administrator/Desktop/githubc/tr/6.py')
-    tr = ("C:/Users/Administrator/Desktop/githubc/tr/tr.txt")
-    result = open(tr,'rb').read()
-    PARAMS_tr1 = {
-            "group_id": H[Z],
-            "message": result,
-
-    }
-    R = S.post(url=URL, params=PARAMS_tr1)
-    DATA = R.json()
-    print(DATA)
-    PARAMS_tr2 = {
-            "group_id": H[Z],
-            "message": L[1],
-
-    }
-        
-    R = S.post(url=URL, params=PARAMS_tr2)
-    DATA = R.json()
-    print(DATA)
-    Z = Z+1
+}
+R = S.post(url=URL, params=PARAMS_2)
+DATA = R.json()
+print(DATA)        
+os.remove(C) 
+os.system('python C:/Users/Administrator/Desktop/githubc/tr/api.py')
