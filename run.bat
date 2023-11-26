120346e8c29737a468ff121b4d597d2a1f01aaec
Marcus-Lacia <65057243+Marcus-Lacia@users.noreply.github.com>
2023-11-27 00:53:31 +0800

    NEWS UPDATE

--- run.bat
+++ run.bat
@@ -10,11 +10,14 @@ move agaim.txt root/github/NEWS
 cd /root/github/NEWS
 node chs.js
 
-
+ls
 cd /root/github/NEWS/beta
+ls
 rename continue *.txt new.txt
 cd /root/github/NEWS/new
+ls
 rename continue *.txt new.txt
+ls
 cd /root/github/NEWS
 
 python diff.py
