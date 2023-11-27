6264700bd4282f9bebbbc3feb61539234f50f8e9
Marcus-Lacia <65057243+Marcus-Lacia@users.noreply.github.com>
2023-11-27 12:10:30 +0800

    NEWS UPDATE

--- diff.py
+++ diff.py
@@ -9,6 +9,7 @@ D = ("new/new.txt")
 B = ("/root/github/NEWS/new/new.txt")
 result = filecmp.cmp(C, D)
 print(result)
+shutil.move(r'beta/new.txt', r'new/new.txt')
 B = result
 if  B:
     print('b是True')
